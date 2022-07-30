de25cb5b89231f2182e87e431df6696c57bb6b7f
mozyakov <s.mozyakov@gmail.com>
2022-07-30 12:16:51 +0700

    11

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,6 +4,6 @@ public class MyFirstProgram {
     public static void main(String[] args) {
 
         System.out.println("Hello, world!");
-        
+        System.out.println("FUck you");
     }
 }
