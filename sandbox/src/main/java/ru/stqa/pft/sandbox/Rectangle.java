d7e78128831ffa76d5f535d8b14fd21a7133144e
mozyakov <s.mozyakov@gmail.com>
2022-07-30 14:31:41 +0700

    1

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by WarDog on 30.07.2022.
+ */
+public class Rectangle {
+    public double a;
+    public double b;
+
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by WarDog on 30.07.2022.
+ */
+public class Square {
+    public double l;
+
+    public Square(double l) {
+        this.l = l;
+    }
+}
